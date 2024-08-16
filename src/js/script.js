c0bf5eb3676868c9efb71c05df07338855d6922b
Erick <erickkan2004@gmail.com>
2024-08-16 15:35:34 -0300

    animation + responsive

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+gsap.from(".image-1", 1.2, {opacity:0, y: 150, delay: 0.5});
+gsap.from(".image-2", 1.2, {opacity:0, y: 150, delay: 1});
+gsap.from(".image-3", 1.2, {opacity:0, y: 150, delay: 1.3});
+gsap.from(".image-4", 1.2, {opacity:0, y: 150, delay: 2});
+gsap.from(".image-5", 1.2, {opacity:0, y: 150, delay: 1.6});
+gsap.from("h1", 1.2, {opacity: 0, y: -80, delay: 1.4});
+gsap.from("p", 1.2, {opacity:0, y: -80, delay: 1.25});
+gsap.from("button", 1.2, {opacity:0, y: -80, delay: 1});
+
+const menu = document.querySelector(".menu");
+const toggle = document.getElementById("toggle");
+
+toggle.addEventListener("click", function(){
+    menu.classList.toggle("active");
+})
+
